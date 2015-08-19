2262f7bee72205de728ad3c82813fe5805bd9b3c
James Hiscock <james@small.mu>
2015-08-19 11:37:59 +1000

    Change where we get name and registry from.
    Docker registry name system conflicts with npm
    name system.

--- docker-tool.js
+++ docker-tool.js
@@ -28,7 +28,7 @@ function main () {
 }
 
 function publish (definition) {
-    var tag = makeTag(definition.registry, definition.name, definition.version)
+    var tag = makeTag(definition)
 
     build(tag, function () {
         push(tag, function () {
@@ -53,7 +53,15 @@ function push (tag, callback) {
     })
 }
 
-function makeTag (registry, name, version) {
+function makeTag (definition) {
+    if (! definition.docker && definition.docker.name) {
+        throw new Error('Needs a name, make sure docker.name exists in package.json')
+    }
+
+    var name = definition.docker.name
+    var registry = definition.docker.registry
+    var version = definition.version
+
     var ret = name
     if (registry) ret = registry + '/' + name
     if (version) ret += ':' + version
